f433bc163d7f0e7c8c84bb7b07101c2777482c96
Alex Cline <rev.a.r.cline@gmail.com>
2021-01-11 15:40:18 -0600

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, 'Mr. Chocolate', 20, 'm', 'brown', 'hungry', 0);
+INSERT INTO bears VALUES (2, 'Rowdy', 10, 'm', 'dark brown', 'hungry', 1);
+INSERT INTO bears VALUES (3, 'Tabitha', 6, 'f', 'dark brown', 'goofy', 1);
+INSERT INTO bears VALUES (4, 'Sargeant Brown', 19, 'm', 'dark brown', 'skittish', 0);
+INSERT INTO bears VALUES (5, 'Melissa', 13, 'f', 'brundle', 'curious', 1);
+INSERT INTO bears VALUES (6, 'Grinch', 2, 'm', 'black', 'ornery', 1);
+INSERT INTO bears VALUES (7, 'Wendy',6, 'f', 'tan', 'friendly', 1);
+INSERT INTO bears VALUES (8, NULL, 20, 'm', 'black', 'aggressive', 0);
--- spec/create_spec.rb
+++ spec/create_spec.rb
@@ -5,31 +5,33 @@ describe 'creating bears table' do
     @sql_runner.execute_create_file
   end
 
-  it "creates a bears table with a name field" do
-    expect{@db.execute("SELECT name FROM bears;")}.to_not raise_exception
+  it 'creates a bears table with a name field' do
+    expect { @db.execute('SELECT name FROM bears;') }.to_not raise_exception
   end
 
-  it "creates a bears table with a age field" do
-    expect{@db.execute("SELECT age FROM bears;")}.to_not raise_exception
+  it 'creates a bears table with a age field' do
+    expect { @db.execute('SELECT age FROM bears;') }.to_not raise_exception
   end
 
-  it "creates a bears table with a sex field" do
-    expect{@db.execute("SELECT sex FROM bears;")}.to_not raise_exception
+  it 'creates a bears table with a sex field' do
+    expect { @db.execute('SELECT sex FROM bears;') }.to_not raise_exception
   end
 
-  it "creates a bears table with a color field" do
-    expect{@db.execute("SELECT color FROM bears;")}.to_not raise_exception
+  it 'creates a bears table with a color field' do
+    expect { @db.execute('SELECT color FROM bears;') }.to_not raise_exception
   end
 
-  it "creates a bears table with a temperament field" do
-    expect{@db.execute("SELECT temperament FROM bears;")}.to_not raise_exception
+  it 'creates a bears table with a temperament field' do
+    expect { @db.execute('SELECT temperament FROM bears;') }.to_not raise_exception
   end
 
-  it "creates a bears table with an alive field" do
-    expect{@db.execute("SELECT alive FROM bears;")}.to_not raise_exception
+  it 'creates a bears table with an alive field' do
+    expect { @db.execute('SELECT alive FROM bears;') }.to_not raise_exception
   end
 
-  it "creates a bears table with an id as a primary key" do
-    expect(@db.execute("PRAGMA table_info(bears);").detect { |arr| arr[-1] == 1 && arr[2] == "INTEGER" }.length).to eq(6)
+  it 'creates a bears table with an id as a primary key' do
+    expect(@db.execute('PRAGMA table_info(bears);').detect do |arr|
+             arr[-1] == 1 && arr[2] == 'INTEGER'
+           end.length).to eq(6)
   end
 end
